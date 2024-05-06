0783b4b71c6ab41a9e5149dde059e3b1bc781365
Matthew Ranford <matt.ranford16@gmail.com>
2024-05-06 13:48:04 +1200

    Updated paragraph with tailiwind changes, initial card-hover-effect created, edited & added to About component

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,6 +1,15 @@
 'use client'
 import { useState, useRef, useEffect } from 'react'
 import { LayoutGrid } from '@/components/ui/layout-grid'
+import { HoverEffect } from '@/components/ui/card-hover-effect'
+import {
+  FcReading,
+  FcGlobe,
+  FcSportsMode,
+  FcFilmReel,
+  FcIdea,
+  FcLandscape,
+} from 'react-icons/fc'
 
 const FrameOne = () => {
   return (
@@ -80,29 +89,68 @@ const cards = [
   },
 ]
 
+const skills = [
+  {
+    text: 'Reading',
+    icon: FcReading,
+  },
+  {
+    text: 'Travel',
+    icon: FcGlobe,
+  },
+  {
+    text: 'Sport',
+    icon: FcSportsMode,
+  },
+  {
+    text: 'Marvel',
+    icon: FcFilmReel,
+  },
+  {
+    text: 'Greenhouse',
+    icon: FcLandscape,
+  },
+  {
+    text: 'Mindfulness',
+    icon: FcIdea,
+  },
+]
+
 export default function About() {
   return (
     <>
-      <section className="md:max-w-full mx-6 lg:mx-14 2xl:mx-36 pt-10 mt-52 2xl:mt-60">
+      <section
+        id="about"
+        className="md:max-w-full mx-6 lg:mx-14 2xl:mx-36 pt-10 mt-52 2xl:mt-60"
+      >
         <div className="space-y-4 2xl:space-y-6">
           <h1 className="text-center text-4xl sm:text-5xl lg:text-7xl 2xl:text-9xl text-yellow-600">
             Who I Am <span>?</span>
           </h1>
-          <div className="flex items-center justify-center">
-            <div className="flex flex-col md:flex-row max-w-7xl 2xl:max-w-full text-xl sm:text-2xl lg:text-3xl pt-20 md:space-x-10  space-y-10 md:space-y-0">
-              <p>
-                {
-                  "I'm a Full-Stack Developer whose recently completed a 17-week intensive boot camp learning full-stack software development at Dev Academy. I've tried various roles in my life, none of them have stuck so this was one of the biggest challenges of my life, diving into the unknown. I still have a lot to learn, but I'm excited to see where my career in tech takes me!"
-                }
+          <p className="text-center text-xl sm:text-2xl lg:text-4xl 2xl:text-6xl">
+            A little bit about me
+          </p>
+          <div className="flex items-center justify-center space-y-10 md:space-y-0 flex-wrap pt-10 2xl:pt-32">
+            <p className="max-w-2xl 2xl:max-w-4xl text-xl sm:text-2xl lg:text-3xl first-letter:text-7xl first-letter:float-start text-left md:text-right 2xl:text-left px-0 md:px-4 2xl:px-10">
+              I&apos;m a Full-Stack Developer who recently completed a 17-week
+              intensive boot camp learning full-stack software development at
+              Dev Academy.
+              <p className="py-2 mt-4">
+                I&apos;ve tried various roles in my life, none of them have
+                stuck so this was one of the biggest challenges of my life,
+                diving into the unknown. I still have a lot to learn, & I&apos;m
+                excited to see where a career in tech takes me!
               </p>
-              <p>
-                {
-                  "I'm a Full-Stack Developer whose recently completed a 17-week intensive boot camp learning full-stack software development at Dev Academy. I've tried various roles in my life, none of them have stuck so this was one of the biggest challenges of my life, diving into the unknown. I still have a lot to learn, but I'm excited to see where my career in tech takes me!"
-                }
-              </p>
+            </p>
+            <div className="max-w-2xl 2xl:max-w-3xl pb-0 md:pb-10">
+              <h2 className="text-center text-2xl text-yellow-600">
+                Some of my other Hobbies
+              </h2>
+              <HoverEffect items={skills} />
             </div>
           </div>
         </div>
+
         <div className="flex lg:flex-row">
           <div className="h-screen py-2 pt-10 w-full">
             <LayoutGrid cards={cards} />
