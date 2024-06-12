aa9e7478b880c797be06e3c60c5635cddb1f1853
Matthew Ranford <matt.ranford16@gmail.com>
2024-06-12 20:12:42 +1200

    Initial implementation of moving border buttons

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -22,23 +22,33 @@ export default function Hero() {
 
   return (
     <>
-      <Image
-        src="/hero-image-test.png"
-        width="2732"
-        height="1536"
-        alt=""
-        className="hero-image"
-      />
-      <h1 className="ps-4 sm:ps-6 lg:ps-8 pt-4 sm:pt-8 md:pt-6 lg:pt-4 xl:pt-3">
-        <TypewriterEffect words={mainHeader} />
-      </h1>
-      <div className="ps-4 sm:ps-6 lg:ps-8">
-        <TextGenerateEffect words={words} />
-      </div>
+      <div className="relative">
+        <h1 className="ps-4 sm:ps-6 lg:ps-8 pt-4 sm:pt-8 md:pt-6 lg:pt-4 xl:pt-3">
+          <TypewriterEffect words={mainHeader} />
+        </h1>
+        <div className="ps-4 sm:ps-6 lg:ps-8">
+          <TextGenerateEffect words={words} />
+        </div>
+        <Image
+          src="/hero-image-test.png"
+          width="2732"
+          height="1536"
+          alt=""
+          className="hero-image"
+        />
+        <div className="absolute text-xl rounded-md p-2 top-[65%] left-[71.5%] space-x-2">
+          <MovingBorderButton className="border border-white">
+            Recent Work!
+          </MovingBorderButton>
+          <MovingBorderButton className="border border-white">
+            Lets Work Together!
+          </MovingBorderButton>
+        </div>
 
-      <h2 className="text-7xl to-sm:text-6xl sm:text-7xl md:text-[5.5rem] lg:text-8xl xl:text-[9.3rem] 2xl:text-[12rem] 3xl:text-[18rem] text-center sm:text-left ps-4 sm:ps-6 lg:ps-8 pt-96 to-sm:pt-96 sm:pt-80 md:pt-72 xl:pt-20 2xl:pt-96 3xl:pt-[38rem] font-bold dark:text-amber-600 text-stone-900">
-        FULL-STACK DEVELOPER
-      </h2>
+        <h2 className="text-7xl to-sm:text-6xl sm:text-7xl md:text-[5.5rem] lg:text-8xl xl:text-[9.3rem] 2xl:text-[12rem] 3xl:text-[18rem] text-center sm:text-left ps-4 sm:ps-6 lg:ps-8 pt-96 to-sm:pt-96 sm:pt-80 md:pt-72 xl:pt-20 2xl:pt-96 3xl:pt-[38rem] font-bold dark:text-amber-600 text-stone-900">
+          FULL-STACK DEVELOPER
+        </h2>
+      </div>
       {/* <main className="">
         <section className="absolute pt-20 sm:pt-32 md:pt-36 lg:pt-24 2xl:pt-40 px-2 md:px-8 2xl:px-10">
           <div className="space-y-4 2xl:space-y-6 text-left"> */}
