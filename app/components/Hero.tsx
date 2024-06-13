66806f2273f77f012b53422b03a12c6263ba3925
Matthew Ranford <matt.ranford16@gmail.com>
2024-06-13 13:09:11 +1200

    Further changes to responsiveness for Hero component

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -31,7 +31,7 @@ export default function Hero() {
         <h1 className="ps-4 sm:ps-6 lg:ps-8 pt-4 sm:pt-8 md:pt-6 lg:pt-4 xl:pt-3">
           <TypewriterEffect words={mainHeader} />
         </h1>
-        <div className="ps-2 sm:ps-6 lg:ps-8 smallest:pt-48 smaller:pt-56 to-sm:pt-48 pt-44">
+        <div className="ps-2 sm:ps-6 lg:ps-8 smallest:pt-48 smaller:pt-56 to-sm:pt-48 small:pt-64 smallAgain:pt-64 smallerAgain:pt-72 sm:pt-80 md:pt-20 xl:pt-6 pt-44">
           <TextGenerateEffect words={firstLine} delay={3000} />
           <TextGenerateEffect words={secondLine} delay={3800} />
           <TextGenerateEffect words={thirdLine} delay={4400} />
@@ -44,15 +44,15 @@ export default function Hero() {
           alt=""
           className="hero-image"
         />
-        <div className="absolute p-2 smallest:top-[35%] smallest:left-[20%] smaller:top-[38%] smaller:left-[28%] to-sm:top-[50%] to-sm:left-[36%] small:top-[48%] small:left-[40%] smallAgain:top-[51%] smallAgain:left-[45%] smallerAgain:top-[55%] smallerAgain:left-[49%] sm:top-[42%] sm:left-[40%] md:top-[30%] md:left-[50%] lg:top-[45%] lg:left-[65%] xl:top-[65%] xl:left-[70%] top-[35%] left-[23%] space-x-2 rotate-6">
-          <MovingBorderButton href="#" className="border border-white">
+        <div className="absolute p-2 smallest:top-[35%] smallest:left-[20%] smaller:top-[38%] smaller:left-[28%] to-sm:top-[50%] to-sm:left-[36%] small:top-[45%] small:left-[40%] smallAgain:top-[48%] smallAgain:left-[45%] smallerAgain:top-[50%] smallerAgain:left-[49%] sm:top-[48%] sm:left-[40%] md:top-[45%] md:left-[50%] lg:top-[60%] lg:left-[60%] xl:top-[68%] xl:left-[72%] top-[35%] left-[23%] space-x-2 rotate-6">
+          <MovingBorderButton href="#" className="">
             Recent Work!
           </MovingBorderButton>
-          <MovingBorderButton href="#" className="border border-white">
+          <MovingBorderButton href="#" className="">
             Lets Work Together!
           </MovingBorderButton>
         </div>
-        <h2 className="text-7xl to-sm:text-[3.3rem] sm:text-7xl md:text-[5.5rem] lg:text-8xl xl:text-[9.3rem] 2xl:text-[12rem] 3xl:text-[18rem] text-right to-sm:text-left sm:text-left ps-4 sm:ps-6 lg:ps-8 pe-4 sm:pe-0 pt-12 to-sm:pt-10 sm:pt-80 md:pt-72 xl:pt-6 2xl:pt-96 3xl:pt-[38rem] font-bold dark:text-amber-600 text-stone-900">
+        <h2 className="text-7xl to-sm:text-[3.3rem] sm:text-7xl md:text-[5.5rem] lg:text-8xl xl:text-[9.3rem] 2xl:text-[12rem] 3xl:text-[18rem] text-right to-sm:text-left sm:text-left ps-4 sm:ps-6 lg:ps-8 pe-4 sm:pe-0 pt-12 to-sm:pt-10 sm:pt-20 md:pt-20 xl:pt-6 2xl:pt-96 3xl:pt-[38rem] font-bold dark:text-amber-600 text-stone-900">
           FULL-STACK DEVELOPER
         </h2>
       </main>
