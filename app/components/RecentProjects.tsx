930a60d576127240df9907dee181c4b51450458e
Matthew Ranford <matt.ranford16@gmail.com>
2024-12-03 17:18:06 +1300

    Initial animation added to RecentProjects component

--- app/components/RecentProjects.tsx
+++ app/components/RecentProjects.tsx
@@ -1,6 +1,10 @@
-import { titan } from '@/fonts'
-import { motion } from 'framer-motion'
+'use client'
+
+import Link from 'next/link'
 import Image from 'next/image'
+import { titan } from '@/fonts'
+import { motion, animate, stagger, useInView } from 'framer-motion'
+import { useEffect, useRef } from 'react'
 import {
   Card,
   CardHeader,
@@ -10,17 +14,40 @@ import {
   Button,
 } from '@nextui-org/react'
 
+// Components
 import { TextGenerateEffectWhileInView } from '@/components/ui/text-generate-effect-while-in-view'
+
+// Images
 import divider from '../../public/images/divider-test-1.png'
 import phoneDivider from '../../public/images/hero-image-divider-1-test.png'
-import Link from 'next/link'
 
+// Text Generate Effect
 const header = 'Recent Projects'
 
 // TODO: Fix hover scale for medium screens etc
 // Put on animation for projects
 
 export default function RecentProjects() {
+  // Project Animation
+  const projectImages = useRef(null)
+  const imagesInView = useInView(projectImages)
+
+  useEffect(() => {
+    if (imagesInView) {
+      animate(
+        '.animate-project-card',
+        { opacity: 1, x: 0 },
+        {
+          duration: 0.5,
+          delay: stagger(0.35, { startDelay: 0.1 }),
+          ease: 'circOut',
+        }
+      )
+    } else {
+      animate('.animate-project-card', { opacity: 0, x: -10 }, { duration: 0 })
+    }
+  }, [imagesInView])
+
   return (
     <>
       <main className="relative pb-10 sm:pb-0">
@@ -68,159 +95,161 @@ export default function RecentProjects() {
             </motion.div>
           </div>
         </div>
-        <div className="max-w-[1200px] xl:max-w-screen-2xl 6xl:max-w-[2500px] gap-4 grid grid-cols-12 grid-rows-2 px-6 mx-auto mt-10 xl:mt-14 pb-6">
-          <Card className="col-span-12 lg:col-span-7 h-[300px] sm:h-[400px] md:h-[450px] lg:h-[400px] 2xl:h-[500px] 6xl:h-[800px] dark:bg-transparent/80 bg-transparent/70">
-            <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-              <p className="text-tiny dark:text-white/60 text-white/80 uppercase dark:font-bold">
-                Web Design & Development
-              </p>
-              <h1 className="text-zinc-300 font-bold text-large">
-                Strictly Paper
-              </h1>
-            </CardHeader>
-            <NextUIImage
-              removeWrapper
-              alt="Latest website project"
-              className="z-0 w-auto h-auto xl:w-full xl:h-full object-cover mt-14 xl:mt-0"
-              src="/images/project-image-test.png"
-            />
-            <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-              <div className="flex flex-grow gap-2 items-center">
-                <div className="flex flex-col">
-                  <p className="text-tiny dark:text-white/60 text-white/100">
-                    Wallpapering company
-                  </p>
-                  <p className="text-tiny dark:text-white/60 text-white/100">
-                    Created from scratch with NextJS.
-                  </p>
+        <motion.div ref={projectImages}>
+          <div className="max-w-[1200px] xl:max-w-screen-2xl 6xl:max-w-[2500px] gap-4 grid grid-cols-12 grid-rows-2 px-6 mx-auto mt-10 xl:mt-14 pb-6">
+            <Card className="col-span-12 lg:col-span-7 h-[300px] sm:h-[400px] md:h-[450px] lg:h-[400px] 2xl:h-[500px] 6xl:h-[800px] dark:bg-transparent/80 bg-transparent/70 animate-project-card">
+              <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+                <p className="text-tiny dark:text-white/60 text-white/80 uppercase dark:font-bold">
+                  Web Design & Development
+                </p>
+                <h1 className="text-zinc-300 font-bold text-large">
+                  Strictly Paper
+                </h1>
+              </CardHeader>
+              <NextUIImage
+                removeWrapper
+                alt="Latest website project"
+                className="z-0 w-auto h-auto xl:w-full xl:h-full object-cover mt-14 xl:mt-0"
+                src="/images/project-image-test.png"
+              />
+              <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
+                <div className="flex flex-grow gap-2 items-center">
+                  <div className="flex flex-col">
+                    <p className="text-tiny dark:text-white/60 text-white/100">
+                      Wallpapering company
+                    </p>
+                    <p className="text-tiny dark:text-white/60 text-white/100">
+                      Created from scratch with NextJS.
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <a
-                href="https://strictly-paper.nz/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button
-                  radius="full"
-                  size="sm"
-                  className="dark:bg-zinc-300 dark:text-stone-800 bg-[#66045f] text-zinc-300"
+                <a
+                  href="https://strictly-paper.nz/"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  View live
-                </Button>
-              </a>
-            </CardFooter>
-          </Card>
-          <Card className="col-span-12 lg:col-span-5 h-[300px] sm:h-[400px] md:h-[450px] lg:h-[400px] 2xl:h-[500px] 6xl:h-[800px] dark:bg-transparent/80 bg-transparent/70 hover:overflow-visible">
-            <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-              <p className="text-tiny dark:text-white/60 text-white/80 uppercase dark:font-bold">
-                Logo & Business Card Design
-              </p>
-              <h1 className="text-zinc-300 font-bold text-large">
-                Attention to Detail Decorators
-              </h1>
-            </CardHeader>
-            <NextUIImage
-              removeWrapper
-              alt="Business card project"
-              className="z-0 w-auto h-auto xl:w-full xl:h-full object-cover mt-14 hover:scale-[1.5] hover:z-20 hover:backdrop-blur-sm hover:-translate-x-2/3 hover:transition hover:ease-in hover:duration-1000"
-              src="/images/atd-business-card.png"
-            />
-            <CardFooter className="absolute bg-black/40 bottom-0 border-t-1 border-default-600 dark:border-default-100">
-              <div className="flex flex-grow gap-2 items-center">
-                <div className="flex flex-col">
-                  <p className="text-tiny dark:text-white/60 text-white/100">
-                    Hover for a better look
-                  </p>
-                  <p className="text-tiny dark:text-white/60 text-white/100">
-                    Created in Canva.
-                  </p>
+                  <Button
+                    radius="full"
+                    size="sm"
+                    className="dark:bg-zinc-300 dark:text-stone-800 bg-[#66045f] text-zinc-300"
+                  >
+                    View live
+                  </Button>
+                </a>
+              </CardFooter>
+            </Card>
+            <Card className="col-span-12 lg:col-span-5 h-[300px] sm:h-[400px] md:h-[450px] lg:h-[400px] 2xl:h-[500px] 6xl:h-[800px] dark:bg-transparent/80 bg-transparent/70 hover:overflow-visible animate-project-card">
+              <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+                <p className="text-tiny dark:text-white/60 text-white/80 uppercase dark:font-bold">
+                  Logo & Business Card Design
+                </p>
+                <h1 className="text-zinc-300 font-bold text-large">
+                  Attention to Detail Decorators
+                </h1>
+              </CardHeader>
+              <NextUIImage
+                removeWrapper
+                alt="Business card project"
+                className="z-0 w-auto h-auto xl:w-full xl:h-full object-cover mt-14 hover:scale-[1.5] hover:z-20 hover:backdrop-blur-sm hover:-translate-x-2/3 hover:transition hover:ease-in hover:duration-1000"
+                src="/images/atd-business-card.png"
+              />
+              <CardFooter className="absolute bg-black/40 bottom-0 border-t-1 border-default-600 dark:border-default-100">
+                <div className="flex flex-grow gap-2 items-center">
+                  <div className="flex flex-col">
+                    <p className="text-tiny dark:text-white/60 text-white/100">
+                      Hover for a better look
+                    </p>
+                    <p className="text-tiny dark:text-white/60 text-white/100">
+                      Created in Canva.
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </CardFooter>
-          </Card>
-          <Card className="col-span-12 lg:col-span-6 h-[300px] sm:h-[400px] md:h-[450px] lg:h-[400px] 2xl:h-[500px] 6xl:h-[800px] dark:bg-transparent/80 bg-transparent/70">
-            <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-              <p className="text-tiny dark:text-white/60 text-white/80 uppercase dark:font-bold">
-                Web Design & Development
-              </p>
-              <h1 className="text-zinc-300 font-bold text-large">
-                McLauchlan Electrical
-              </h1>
-            </CardHeader>
-            <NextUIImage
-              removeWrapper
-              alt="Second website project prototype"
-              className="z-0 w-auto h-auto xl:w-full xl:h-full object-cover mt-14 xl:mt-0"
-              src="/images/project-image-test-1.png"
-            />
-            <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-              <div className="flex flex-grow gap-2 items-center">
-                <div className="flex flex-col">
-                  <p className="text-tiny dark:text-white/60 text-white/100">
-                    Prototype for an electrical company
-                  </p>
-                  <p className="text-tiny dark:text-white/60 text-white/100">
-                    Created from scratch with NextJS.
-                  </p>
+              </CardFooter>
+            </Card>
+            <Card className="col-span-12 lg:col-span-6 h-[300px] sm:h-[400px] md:h-[450px] lg:h-[400px] 2xl:h-[500px] 6xl:h-[800px] dark:bg-transparent/80 bg-transparent/70 animate-project-card">
+              <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+                <p className="text-tiny dark:text-white/60 text-white/80 uppercase dark:font-bold">
+                  Web Design & Development
+                </p>
+                <h1 className="text-zinc-300 font-bold text-large">
+                  McLauchlan Electrical
+                </h1>
+              </CardHeader>
+              <NextUIImage
+                removeWrapper
+                alt="Second website project prototype"
+                className="z-0 w-auto h-auto xl:w-full xl:h-full object-cover mt-14 xl:mt-0"
+                src="/images/project-image-test-1.png"
+              />
+              <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
+                <div className="flex flex-grow gap-2 items-center">
+                  <div className="flex flex-col">
+                    <p className="text-tiny dark:text-white/60 text-white/100">
+                      Prototype for an electrical company
+                    </p>
+                    <p className="text-tiny dark:text-white/60 text-white/100">
+                      Created from scratch with NextJS.
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <a
-                href="https://matthew-ranford.github.io/mclauchlan-electrical/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button
-                  radius="full"
-                  size="sm"
-                  className="dark:bg-zinc-300 dark:text-stone-800 bg-[#66045f] text-zinc-300"
+                <a
+                  href="https://matthew-ranford.github.io/mclauchlan-electrical/"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  View live
-                </Button>
-              </a>
-            </CardFooter>
-          </Card>
-          <Card className="col-span-12 lg:col-span-6 h-[300px] sm:h-[400px] md:h-[450px] lg:h-[400px] 2xl:h-[500px] 6xl:h-[800px] dark:bg-transparent/80 bg-transparent/70">
-            <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-              <p className="text-tiny dark:text-white/60 text-white/80 uppercase dark:font-bold">
-                Web Design & Development
-              </p>
-              <h1 className="text-zinc-300 font-bold text-large">
-                Attention to Detail Decorators
-              </h1>
-            </CardHeader>
-            <NextUIImage
-              removeWrapper
-              alt="First website project"
-              className="z-0 w-auto h-auto xl:w-full xl:h-full object-cover mt-14 xl:mt-0"
-              src="/images/project-image-test-2.png"
-            />
-            <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-              <div className="flex flex-grow gap-2 items-center">
-                <div className="flex flex-col">
-                  <p className="text-tiny dark:text-white/60 text-white/100">
-                    Painting & decorating company
-                  </p>
-                  <p className="text-tiny dark:text-white/60 text-white/100">
-                    Created from scratch with React.
-                  </p>
+                  <Button
+                    radius="full"
+                    size="sm"
+                    className="dark:bg-zinc-300 dark:text-stone-800 bg-[#66045f] text-zinc-300"
+                  >
+                    View live
+                  </Button>
+                </a>
+              </CardFooter>
+            </Card>
+            <Card className="col-span-12 lg:col-span-6 h-[300px] sm:h-[400px] md:h-[450px] lg:h-[400px] 2xl:h-[500px] 6xl:h-[800px] dark:bg-transparent/80 bg-transparent/70 animate-project-card">
+              <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+                <p className="text-tiny dark:text-white/60 text-white/80 uppercase dark:font-bold">
+                  Web Design & Development
+                </p>
+                <h1 className="text-zinc-300 font-bold text-large">
+                  Attention to Detail Decorators
+                </h1>
+              </CardHeader>
+              <NextUIImage
+                removeWrapper
+                alt="First website project"
+                className="z-0 w-auto h-auto xl:w-full xl:h-full object-cover mt-14 xl:mt-0"
+                src="/images/project-image-test-2.png"
+              />
+              <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
+                <div className="flex flex-grow gap-2 items-center">
+                  <div className="flex flex-col">
+                    <p className="text-tiny dark:text-white/60 text-white/100">
+                      Painting & decorating company
+                    </p>
+                    <p className="text-tiny dark:text-white/60 text-white/100">
+                      Created from scratch with React.
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <a
-                href="https://atd-decorators.co.nz/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button
-                  radius="full"
-                  size="sm"
-                  className="dark:bg-zinc-300 dark:text-stone-800 bg-[#66045f] text-zinc-300"
+                <a
+                  href="https://atd-decorators.co.nz/"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  View live
-                </Button>
-              </a>
-            </CardFooter>
-          </Card>
-        </div>
+                  <Button
+                    radius="full"
+                    size="sm"
+                    className="dark:bg-zinc-300 dark:text-stone-800 bg-[#66045f] text-zinc-300"
+                  >
+                    View live
+                  </Button>
+                </a>
+              </CardFooter>
+            </Card>
+          </div>
+        </motion.div>
         <div className="pt-6 text-center font-normal dark:font-bold 6xl:text-3xl">
           <button className="border-3 border-stone-800 rounded-xl bg-[#66045f] transition-all duration-1000 dark:hover:bg-stone-800 dark:hover:text-[#004aad] hover:bg-zinc-300 dark:text-zinc-300 text-stone-900 hover:text-[#004aad] drop-shadow-[4px_4px_0px_#1c1917] py-3 px-3 sm:px-4 sm:py-4 6xl:px-8 font-bold">
             <Link href="projects">View all projects</Link>
